9e9f72a056f0caa2aca916e965b129c5a0edea6f
35-hasib <hrrahman35@gmail.com>
2025-01-01 18:14:06 +0600

    day 1

---
+++ Python/test.py
@@ -0,0 +1,37 @@
+
+# i = 0
+# while True:
+
+#     n = int(input('Enter a number : '))
+#     if n & 1 :
+#         print('Number is Odd !')
+#     else:
+#         print('Number is Even !')
+#     i = i+1
+#     if i == 10:
+#         break
+
+
+
+sum = 10 +\
+20-\
+4
+print(f'{sum} is Faltu""')
+
+
+a = '''flkighgfkhgkgfjhgfkhfkhgf
+ghhgjgfd
+hg
+lgkhf
+lgkh
+gkhdfg
+khf''""""
+gkhdflgkhdf
+glkdhfg
+dhfgl]
+hdfg
+ldhf
+
+
+'''
+print(a)

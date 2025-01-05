8efcad8b9ea65170aa71c1875d8433cf585b5782
35-hasib <hrrahman35@gmail.com>
2025-01-05 17:29:12 +0600

    Day 3

---
+++ Python/Class_03.py
@@ -0,0 +1,20 @@
+# print(bin(12))
+# print(bin(12 >> 1))
+# print(int('00001100',2))
+
+# a = 60
+# b = 13
+
+# print(f'{a&b} {a|b} {a^b} {~a} {a<<2} {a>>2}')
+# print(f'{bin(a&b)} {bin(a|b)} {bin(a^b)} {bin(~a)} {bin(a<<2)} {bin(a>>2)}')
+
+# print(5//2)
+# print(5/2)
+
+# a = 2
+# b = 1.4
+# print(type(a-b))
+
+a = 6 + 4j
+b = 3 + 6j
+print(abs(a-b))

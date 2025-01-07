060ae8fa8df6789eb898b102c9f44705e31cb652
35-hasib <hrrahman35@gmail.com>
2025-01-07 18:47:48 +0600

    day 05

--- Python/test.py
+++ Python/test.py
@@ -44,41 +44,9 @@
 
 # # Task 05
 
-# n = 20
-# for i in range(n):
-#     if((i+1)%3 == 0):
-#         continue
-#     else:
-#         print(i+1, end=' ')
-
-
-
-# h = input('Enter Hello : ')
-# for i in h:
-#     print(i,end=",")
-
-# list = [1,2,3,4]
-
-# for i,j in enumerate(list):
-#     print(i,j)
-
-# Login
-
-list_id = ['hasib','tarik','zisan','abir','zisan','touhid']
-list_pass = ['11','22','33','44','55','66']
-
-id = input('ID : ')
-if(id in list_id):
-    for i in range(len(list_id)):
-        if(list_id[i] == id):
-            pas = input('Password : ')
-            if(list_pass[i] == pas):
-                print('Loged in')
-                exit()
-            else:
-                print('Invalid Password !!')
-                break
-else:
-    print('Invalid ID !!')
-
-
+n = 20
+for i in range(n):
+    if((i+1)%3 == 0):
+        continue
+    else:
+        print(i+1, end=' ')

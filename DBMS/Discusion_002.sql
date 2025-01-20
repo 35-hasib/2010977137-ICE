53256d2cf67f39a46ef5a3f8b4999b5b4404df60
35-hasib <hrrahman35@gmail.com>
2025-01-20 23:24:04 +0600

    add discussion_002.sql and create table

---
+++ DBMS/Discusion_002.sql
@@ -0,0 +1,27 @@
+-- 01
+select * from publishers
+select * from authors
+select * from titleauthor
+select * from titles
+
+select au_lname, title_id from titleauthor join authors on titleauthor.au_id = authors.au_id
+
+-- task 01
+--i
+select au_lname, title from authors join titleauthor on authors.au_id = titleauthor.au_id join titles on titles.title_id = titleauthor.title_id
+--ii
+select au_lname, title,pub_name from authors join titleauthor on authors.au_id = titleauthor.au_id join titles on titles.title_id = titleauthor.title_id join publishers on publishers.pub_id = titles.pub_id
+
+-- 02
+select au_lname, pub_name from authors,publishers
+
+-- task 02
+select au_lname,pub_name,authors.city from authors join publishers on authors.city = publishers.city
+
+-- 03 
+select * from titles where royalty = (select avg(royalty)  from titles)
+
+-- Task 03
+
+
+ 
--- DBMS/create_table.sql
+++ DBMS/create_table.sql
@@ -1,14 +1,38 @@
-create table test001(
-	student_id varchar(10) primary key,
-	name varchar(20) not null,
-	age int not null,
-	relation_status varchar(20)
-)
 
-insert into test001 values
-('2010977137', 'Hasib', 23, 'Single'),
-('2011077117', 'Abir', 24, 'In a relation'),
-('2010177123', 'Tarik', 22, 'In a relation'),
-('2010277155', 'Touhid', 22, 'Mingle');
 
-select * from test001
+-- Creating table
+CREATE TABLE CustomerAndSuppliers (
+    cust_id varCHAR(6) PRIMARY KEY CHECK (cust_id LIKE '[CS][0-9][0-9][0-9][0-9][0-9]'),
+    cust_fname varCHAR(15) NOT NULL,
+    cust_lname VARCHAR(15),
+    cust_address TEXT,
+    cust_telno CHAR(12) CHECK (cust_telno LIKE '[0-9][0-9][0-9][-][0-9][0-9[0-9][0-9][0-9][0-9][0-9][0-9]'),
+    cust_city CHAR(12) DEFAULT 'Rajshahi',
+    sales_amnt MONEY CHECK (sales_amnt >= 0),
+    proc_amnt MONEY CHECK (proc_amnt >= 0)
+);
+
+
+-- Creating the Item table
+CREATE TABLE Item (
+    item_id CHAR(6) PRIMARY KEY CHECK (item_id LIKE 'P[0-9][0-9][0-9][0-9][0-9]'),
+    item_name CHAR(12),
+    item_category CHAR(10) CHECK (item_category IN ('Electrical', 'Mechanical', 'Software', 'Books')),
+    item_price FLOAT CHECK (item_price >= 0),
+    item_qty INT CHECK (item_qty >= 0),
+    item_last_sold DATE DEFAULT GETDATE()
+);
+
+-- Creating the Transactions table
+CREATE TABLE Transactions (
+    tran_id CHAR(10) PRIMARY KEY CHECK (tran_id LIKE 'T[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]'),
+    item_id CHAR(6) FOREIGN KEY REFERENCES Item(item_id),
+    cust_id CHAR(6), -- Assuming there's a Customer table; reference can be added if needed
+    tran_type CHAR(1) CHECK (tran_type IN ('S', 'O')),
+    tran_quantity INT CHECK (tran_quantity > 0),
+    tran_date DATETIME DEFAULT GETDATE()
+);
+
+select * from CustomerAndSuppliers
+select * from Item
+select * from Transactions
